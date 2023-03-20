983d06c53d68062bc937f84ac7325a42baad38dd
sufiyan0 <86568496+sufiyan0@users.noreply.github.com>
2023-03-20 20:03:49 +0500

    todo app

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoWraper from './components/TodoWraper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWraper />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/components/TodoWraper.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {TodoForm} from './TodoForm';
+
+const TodoWraper = () => {
+  return (
+    <div className='TodoWrapper'>
+      <TodoForm />
+    </div>
+  )
+}
+
+export default TodoWraper
+
